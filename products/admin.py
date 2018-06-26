29732604fc7e73ce0f6c665a132888eb852f39b5
Wai loon Chong <derrickc0527@Wais-MacBook-Pro.local>
2018-06-26 10:54:53 -0400

    remove token

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Products
+# from import_export.admin import ImportExportModelAdmin
 
 admin.site.register(Products)
+
+# @admin.register(Products)
+# class ViewAdmin(ImportExportModelAdmin):
+#     pass
